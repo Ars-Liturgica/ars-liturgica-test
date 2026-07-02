04b9a65022b7ece75a6e0c5a606347805d4bb1eb
Ars-Liturgica <leonardorazzi.01@gmail.com>
2026-07-02 12:09:46 +0200

    Create AreaParrocchiale.jsx
    Creata struttura Area Parrocchiale

---
+++ frontend/src/parrocchia/AreaParrocchiale.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RegistrazioneParroco from "./RegistrazioneParroco";
+
+export default function AreaParrocchiale() {
+  return (
+    <div>
+      <RegistrazioneParroco />
+    </div>
+  );
+}
