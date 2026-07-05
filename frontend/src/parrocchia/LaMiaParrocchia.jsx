4143e9879ad2b1ecdfe861d9a6685dadf68e742d
Ars-Liturgica <leonardorazzi.01@gmail.com>
2026-07-05 18:33:29 +0200

    Create LaMiaParrocchia.jsx
    Crea componente LaMiaParrocchia

---
+++ frontend/src/parrocchia/LaMiaParrocchia.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function LaMiaParrocchia() {
+  return (
+    <div>
+      <h1>Benvenuto nella tua Parrocchia</h1>
+
+      <p>
+        Presentati alla tua comunità parrocchiale. Bastano pochi dati per
+        iniziare a vivere gli strumenti e i servizi che la tua parrocchia mette
+        a disposizione attraverso Ars Liturgica.
+      </p>
+    </div>
+  );
+}
