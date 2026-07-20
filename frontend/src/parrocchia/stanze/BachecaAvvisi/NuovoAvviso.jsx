a8ddd13043034ea327cb54d22a19d3182ab10dcb
Ars-Liturgica <leonardorazzi.01@gmail.com>
2026-07-20 17:37:53 +0200

    Update NuovoAvviso.jsx

--- frontend/src/parrocchia/stanze/BachecaAvvisi/NuovoAvviso.jsx
+++ frontend/src/parrocchia/stanze/BachecaAvvisi/NuovoAvviso.jsx
@@ -743,7 +743,7 @@ const [categoriaPersonalizzata, setCategoriaPersonalizzata] = useState("");
                     <p>
                       {firma === "parroco"
                         ? "Il Parroco"
-                       : nomeParrocchia || "La Parrocchia"
+                       : nomeParrocchia || "La Parrocchia"}
                     </p>
                   </div>
                 )}
